7a588f14dc5b529f5dbe47dfbbd7f04bcaf3a656
SavageLM <nagol.egavas@gmail.com>
2024-08-27 14:59:34 -0500

    Added code to 103-exponential

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -12,7 +12,7 @@
 
 /*Prototypes*/
 int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t min, size_t max, size_t size, int value);
 void print_search(int *array, size_t min, size_t max);
 
 int interpolation_search(int *array, size_t size, int value);
